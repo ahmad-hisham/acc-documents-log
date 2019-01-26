3fd523a99118724452da51e0f82eb0b9bc9f4f9d
Ahmad Hisham <ahmad.hisham@gmail.com>
2019-01-27 00:06:02 +0300

    Create Forge Viewer static page

--- start.js
+++ start.js
@@ -3,6 +3,9 @@ let app = express();
 
 const path = require("path");
 
+// enable static http serving
+app.use(express.static(path.join(__dirname, "public")));
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");

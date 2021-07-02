762bcce99418393cbf5b5ae491dac8e795fc3ed6
Ahmad Hisham <me@ahmadhisham.me>
2021-07-02 22:58:32 +0300

    Change App name to "ACC Documents Log"

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET login page. */
 router.get("/", function(req, res) {
-  res.render("index", { title: "Forge App", contents: "Welcome to Forge Lab" });
+  res.render("index", { title: "ACC Documents Log", contents: "Welcome to ACC Documents Log Exporter" });
 });
 
 module.exports = router;
